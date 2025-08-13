26bae262cee0b1320c3c4a06775121037cbf2016
Qian <ali_zhqan@aliyun.com>
2025-08-13 10:11:59 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,28 +1,72 @@
+import sys
 from flask import Flask, render_template
 from flask import url_for
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN: # 如果是Windows系统，使用///
+    prefix = 'sqlite:///'
+else: # 否则使用////
+    prefix = 'sqlite:////'
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型修改的监控
+#在扩展类实例化前加载配置
+db  = SQLAlchemy(app)
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+class User(db.Model): #表名将会是user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'Grey Li'
+    movies = [
+                 {'title': 'My Neighbor Totoro', 'year': '1988'},
+                 {'title': 'Dead Poets Society', 'year': '1989'},
+                 {'title': 'A Perfect World', 'year': '1993'},
+                 {'title': 'Leon', 'year': '1994'},
+                 {'title': 'Mahjong', 'year': '1996'},
+                 {'title': 'Swallowtail Butterfly', 'year': '1996'},
+                 {'title': 'King of Comedy', 'year': '1999'},
+                 {'title': 'Devils on the Doorstep', 'year': '1999'},
+                 {'title': 'WALL-E', 'year': '2008'},
+                 {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user= User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done')
+
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database')
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
-def hello():
+#def hello():
 #    return "Welcome to My Watchlist!"
-    return render_template('index.html', name=name, movies=movies)
+#   return render_template('index.html', name=name, movies=movies)
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
