60d53d25263b961041b52f6560f85ac575218d1e
Qian <ali_zhqan@aliyun.com>
2025-08-13 15:56:09 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -57,6 +57,15 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database')
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
@@ -64,22 +73,21 @@ def initdb(drop):
 #    return "Welcome to My Watchlist!"
 #   return render_template('index.html', name=name, movies=movies)
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return  f'User:{escape(name)}'
+#@app.route('/user/<name>')
+#def user_page(name):
+#    return  f'User:{escape(name)}'
 
-@app.route('/test')
-def test_url_for():
+#@app.route('/test')
+#def test_url_for():
     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+#    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
+ #   print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+  #  print(url_for('user_page', name='peter'))  # 输出：/user/peter
+   # print(url_for('test_url_for'))  # 输出：/test
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
+    #print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    #return 'Test page'
